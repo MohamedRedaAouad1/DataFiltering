d49485db0c77d541435c1aea787ab44447342a1a
pycharm <pycharm@Air-de-Mohamed.lan>
2023-03-17 21:22:44 +0100

    Initial commit

---
+++ kalman filter.py
@@ -0,0 +1,95 @@
+
+DataSet=['y: -0.9795349836349481',' currentTs: 1669791083.7107968',' sensorName: gravity  Non-wakeup','x: -0.064577542245388','z: -0.19063392281532202','y: -0.169528305530548',' currentTs: 1669791083.7107968',' sensorName: linear_acceleration','x: 0.0963921174407','z: 0.21620765328407202','y: -0.37373092770576405',' currentTs: 1669791083.7107968',' sensorName: Rotation Vector  Non-wakeup','x: -0.325501441955566','z: 0.272466510534286','y: -43.01274490356445',' currentTs: 1669791083.7107968',' sensorName: ak0991x Magnetometer Non-wakeup','x: -17.24371337890625','z: 1.627761840820312',' accuracy: high',' altitude: location-update',' altitude: 0',' currentTs: 1669791083.9999988',' sensorName: GPS',' lon: 7.696818185644629',' direction: 286.1669877781809',' lat: 48.73642270716055',' accuracy: 4.747088487954784',' altitude: 145.31895648874342','y: -0.997424244880676',' currentTs: 1669791084.710754',' sensorName: gravity  Non-wakeup','x: -0.023097770288586002','z: -0.06790655106306001','y: 0.054522693157196',' currentTs: 1669791084.710754',' sensorName: linear_acceleration','x: 0.13033655285835202','z: 0.08093751221895201','y: -0.700703024864196',' currentTs: 1669791084.710754',' sensorName: Rotation Vector  Non-wakeup','x: -1.146238565444946','z: -0.013764522969722002','y: -43.13346481323242',' currentTs: 1669791084.710754',' sensorName: ak0991x Magnetometer Non-wakeup','x: -14.786956787109375','z: 8.027908325195312',' accuracy: high',' altitude: location-update',' altitude: 0',' currentTs: 1669791084.9999986',' sensorName: GPS',' lon: 7.696834961898929',' direction: 286.1670003101475',' lat: 48.73641648744988',' accuracy: 4.746126233874172',' altitude: 145.66175147611648','y: -0.9915364384651181',' currentTs: 1669791085.710752',' sensorName: gravity  Non-wakeup','x: -0.048651479184627006','z: 0.120368175208568','y: 0.004201292991638001',' currentTs: 1669791085.710752',' sensorName: linear_acceleration','x: 0.101813107728958','z: 0.033577792346477','y: 0.360383063554763',' currentTs: 1669791085.710752',' sensorName: Rotation Vector  Non-wakeup','x: -0.7811557054519651','z: -0.38414579629898005','y: -40.54121780395508',' currentTs: 1669791085.710752',' sensorName: ak0991x Magnetometer Non-wakeup','x: -17.201400756835938','z: 14.279388427734375',' accuracy: high',' altitude: location-update',' altitude: 0',' currentTs: 1669791085.9999993',' sensorName: GPS',' lon: 7.696848611622223',' direction: 286.16701050227266',' lat: 48.736411261744365',' accuracy: 4.743948233787084',' altitude: 145.76313196402043','y: -0.9481539130210871',' currentTs: 1669791086.710743',' sensorName: gravity  Non-wakeup','x: -0.107431516051292','z: 0.29910299181938105','y: 0.196200788021087',' currentTs: 1669791086.710743',' sensorName: linear_acceleration','x: 0.209344968199729','z: -0.34696984291076605','y: -0.927439868450164',' currentTs: 1669791086.710743',' sensorName: Rotation Vector  Non-wakeup','x: -1.063999891281128','z: -0.9150306582450861','y: -35.888580322265625',' currentTs: 1669791086.710743',' sensorName: ak0991x Magnetometer Non-wakeup','x: -20.3819580078125','z: 19.13958740234375',' accuracy: high',' altitude: location-update',' altitude: 0',' currentTs: 1669791086.9999988',' sensorName: GPS',' lon: 7.696859754877641',' direction: 286.1670188076387',' lat: 48.73640641209807',' accuracy: 4.748471411739442',' altitude: 146.0610543983057','y: -0.9546125531196591',' currentTs: 1669791087.7107959',' sensorName: gravity  Non-wakeup','x: -0.08420986682176501','z: 0.285698533058166','y: 0.188331425189971',' currentTs: 1669791087.7107959',' sensorName: linear_acceleration','x: -0.22595553100109103','z: 0.10857331752777101','y: -0.7969197034835811',' currentTs: 1669791087.7107959',' sensorName: Rotation Vector  Non-wakeup','x: -2.984620094299316','z: 0.27889764308929405','y: -36.76664352416992',' currentTs: 1669791087.7107959',' sensorName: ak0991x Magnetometer Non-wakeup','x: -19.18310546875','z: 19.053726196289062',' accuracy: high',' altitude: location-update',' altitude: 0',' currentTs: 1669791087.999999',' sensorName: GPS',' lon: 7.696867854090198',' direction: 286.16702487',' lat: 48.736403877705825',' accuracy: 4.742888311282407',' altitude: 145.66022664867342','y: -0.864002227783203',' currentTs: 1669791088.7108011',' sensorName: gravity  Non-wakeup','x: -0.14776280522346402','z: 0.48131760954856806','y: 0.262851595878601',' currentTs: 1669791088.7108011',' sensorName: linear_acceleration','x: -0.073855862021446','z: -0.315057814121246','y: 1.455488801002502',' currentTs: 1669791088.7108011',' sensorName: Rotation Vector  Non-wakeup','x: 0.636313736438751','z: -0.355039924383163','y: -31.438867568969727',' currentTs: 1669791088.7108011',' sensorName: ak0991x Magnetometer Non-wakeup','x: -23.226715087890625','z: 23.478012084960938',' accuracy: high',' altitude: location-update',' altitude: 0',' currentTs: 1669791088.9999986',' sensorName: GPS',' lon: 7.696879488241412',' direction: 286.1670335496421',' lat: 48.73639913686007',' accuracy: 4.747504281050269',' altitude: 145.76591846719384','y: -0.9559662342071531',' currentTs: 1669791089.710748',' sensorName: gravity  Non-wakeup','x: -0.07893519848585101','z: 0.282661855220794','y: 0.000796556472778',' currentTs: 1669791089.710748',' sensorName: linear_acceleration','x: -0.204359471797943','z: -0.331535816192626','y: 1.190247535705566',' currentTs: 1669791089.710748',' sensorName: Rotation Vector  Non-wakeup','x: 1.874234795570373','z: 0.11156675219535801','y: -37.78899383544922',' currentTs: 1669791089.710748',' sensorName: ak0991x Magnetometer Non-wakeup','x: -20.8870849609375','z: 16.735122680664062',' accuracy: high',' altitude: location-update',' altitude: 0.87409930029208',' currentTs: 1669791089.999999',' sensorName: GPS',' lon: 7.696884887544768',' direction: -1',' lat: 48.7364021683575',' accuracy: 4.742411843431221',' altitude: 143.81837811972946','y: -0.9952592253684991',' currentTs: 1669791090.71075',' sensorName: gravity  Non-wakeup','x: -0.09670118242502201','z: 0.010405869223177001','y: -0.007136523723602001',' currentTs: 1669791090.71075',' sensorName: linear_acceleration','x: 0.05420544743537901','z: -0.17454469203948902','y: 1.382655143737793',' currentTs: 1669791090.71075',' sensorName: Rotation Vector  Non-wakeup','x: 3.514060735702514','z: -0.006558642722666001','y: -41.27257537841797',' currentTs: 1669791090.71075',' sensorName: ak0991x Magnetometer Non-wakeup','x: -21.281051635742188','z: 8.495956420898438',' accuracy: high',' altitude: location-update',' altitude: 0.603360433055647',' currentTs: 1669791090.999999',' sensorName: GPS',' lon: 7.696904031308186',' direction: 111.30925511353529',' lat: 48.73639542737248',' accuracy: 4.740600504112665',' altitude: 144.95967572834343','y: -0.9642504453659051',' currentTs: 1669791091.710791',' sensorName: gravity  Non-wakeup','x: -0.09689368307590401','z: -0.24664321541786102','y: -0.349668741226196',' currentTs: 1669791091.710791',' sensorName: linear_acceleration','x: 0.19416843354701901','z: 0.38299575448036105','y: -0.9826949834823601',' currentTs: 1669791091.710791',' sensorName: Rotation Vector  Non-wakeup','x: 0.15396209061145702','z: -0.017459418624639','y: -42.61799621582031',' currentTs: 1669791091.710791',' sensorName: ak0991x Magnetometer Non-wakeup','x: -21.047622680664062','z: -1.368606567382812',' accuracy: high']
+répertoire=[]
+répertoire1=[]
+vitesse=[]
+def data(DataSet):#on extrait x,y,z du dataset
+ test ='x'
+ res = [idx for idx in DataSet if idx[0].lower() == test.lower()]
+
+ return res
+def data2(DataSet):
+ test2='y'
+
+ res1 = [idx for idx in DataSet if idx[0].lower() == test2.lower()]
+
+ return res1
+
+def data3(DataSet):
+
+ test3='z'
+ res2 = [idx for idx in DataSet if idx[0].lower() == test3.lower()]
+ return res2
+j1=data(DataSet)
+j2=data2(DataSet)
+j3=data3(DataSet)
+X_1=[s.strip('x: ') for s in j1]
+Y_2=[s.strip('y: ') for s in j2]
+Z_3=[s.strip('z: ') for s in j3]
+#on garde seulement les valeurs
+import numpy as np
+from math import *
+#initialisation de notre filtre de kalman
+#on définit la matrice de transition d'état
+dt = 1.0  #time step
+A = np.array([[1, 0, 0, dt, 0, 0],
+              [0, 1, 0, 0, dt, 0],
+              [0, 0, 1, 0, 0, dt],
+              [0, 0, 0, 1, 0, 0],
+              [0, 0, 0, 0, 1, 0],
+              [0, 0, 0, 0, 0, 1]])
+
+#on définit la matrice d'observation
+H = np.array([[1, 0, 0, 0, 0, 0],
+              [0, 1, 0, 0, 0, 0],
+              [0, 0, 1, 0, 0, 0]])
+
+#on définit les matrices de covariance
+Q = np.eye(6) * 0.01  # process noise
+R = np.eye(3) * 10  # measurement noise
+
+#on définit la matrice d'estimation d'état initial et la matrice de covariance
+x = np.array([float(X_1[0]), float(Y_2[0]), float(Z_3[0]),0,0,0])
+x= np.expand_dims(x, axis=-1)#on réajuste la dimension de la matrice par souci de calcul
+P = np.eye(6) * 100  # covariance initiale
+
+#on loop à travers nos mesures du sensor du téléphone
+for i in range(len(X_1)):
+    # on prédit l'état et on définit la nouvelle matrice de covariance avec la matrice de transition d'état
+    x = np.dot(A, x)
+    P = np.dot(np.dot(A, P), A.T) + Q
+
+    # Update de la matrice d'état et de covariance avec nos mesures du sensor
+    z = np.array([[float(X_1[i])],
+                 [float(Y_2[i])],
+                 [float(Z_3[i])]])
+    y = z - np.dot(H, x)
+    S = np.dot(np.dot(H, P), H.T) + R
+    K = np.dot(np.dot(P, H.T), np.linalg.inv(S))
+    x = x + np.dot(K, y)
+    P = np.dot(np.eye(6) - np.dot(K, H), P)
+
+    #on print l'état estimé
+    print("Estimated state:", x[:6])
+    vitesse.append(18*(sqrt((x[3][0])**2+(x[4][0])**2+(x[5][0])**2))/5) #on avait décomposé le vecteur vitesse par combinaison linéaire
+                                                               #vx,vy,vz, on cherche une valeur : la vitesse, on utilise
+                                                               #la formule de la norme des vecteurs
+print(vitesse)                                                 #les données du gps sont en m/s, on convertit en km/h
+                                                               #j'obtient des résultats cohérents,vitesse moyenne piéton : 3,6km/h
+#accélération brusque, freinage brusque
+for j in range(len(vitesse)-1):
+ if vitesse[j+1]-vitesse[j]>3 :
+    a=j+1
+    répertoire.append(str(a))
+    répertoire.append(":accélération brusque")
+ if vitesse[j + 1] - vitesse[j] <- 3:
+    a = j + 1
+    répertoire1.append(str(a))
+    répertoire1.append(":freinage brusque")
+
+
+print(répertoire)
+print(répertoire1)
+
+
+
